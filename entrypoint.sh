4815aa73ccc335d16fdb3224911b327fd0b76a50
fh <finley.d.hammond@outlook.com>
2025-06-11 21:35:48 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(cat /run/secrets/airflow_pg_conn)
-export AIRFLOW__CELERY__RESULT_BACKEND=$(cat /run/secrets/airflow_celery_pg_conn)
-export AIRFLOW__CELERY__BROKER_URL=$(cat /run/secrets/airflow_celery_redis_conn)
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(< /run/secrets/airflow_pg_conn)
+export AIRFLOW__CELERY__RESULT_BACKEND=$(< /run/secrets/airflow_celery_pg_conn)
+export AIRFLOW__CELERY__BROKER_URL=$(< /run/secrets/airflow_celery_redis_conn)
 
-exec "$@"
+# Run the real airflow command
+exec airflow "$@"
