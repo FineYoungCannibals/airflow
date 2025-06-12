fda6c73286afe512f65ffcc7d6af8754d0316f7e
fh <finley.d.hammond@outlook.com>
2025-06-11 21:25:20 -0400

    Update entrypoint.sh
    updating entrypoint, trying this script to load the environment variables then accept the airflow command and params

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(cat $AIRFLOW__DATABASE__SQL_ALCHEMY_CONN_FP)
-export AIRFLOW__CELERY__RESULT_BACKEND=$(cat $AIRFLOW__CELERY__RESULT_BACKEND_FP)
-export AIRFLOW__CELERY__BROKER_URL=$(cat $AIRFLOW__CELERY__BROKER_URL_FP)
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=$(cat /run/secrets/airflow_pg_conn)
+export AIRFLOW__CELERY__RESULT_BACKEND=$(cat /run/secrets/airflow_celery_pg_conn)
+export AIRFLOW__CELERY__BROKER_URL=$(cat /run/secrets/airflow_celery_redis_conn)
 
-exec /entrypoint "$@"
+exec "$@"
